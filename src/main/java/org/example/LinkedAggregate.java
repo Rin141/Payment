e46750f0a1d2b0c2175712883f3e447a3d7355f2
Rin141 <thanhlt@hopee.com.vn>
2024-09-25 12:57:07 +0700

    3 commmit

--- src/main/java/org/example/LinkedAggregate.java
+++ src/main/java/org/example/LinkedAggregate.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.List;
 
 public class LinkedAggregate extends Login {
     private WebElement driver;
@@ -17,15 +16,15 @@ public class LinkedAggregate extends Login {
 
     @BeforeClass
     public void setUpAndLogin() {
-        setup();  // Gọi setup từ lớp cha (nếu setup đã khởi tạo driver)
-        login();  // Gọi login từ lớp cha để đăng nhập
+        setup();
+        login();
     }
 
     @Test
     public void edit_button() {
-        setup();  // Gọi setup từ lớp cha (nếu setup đã khởi tạo driver)
-        login();  // Gọi login từ lớp cha để đăng nhập
-        sleep(2000); // Chờ để đảm bảo đăng nhập xong
+        setup();
+        login();
+        sleep(2000);
         // Kiểm tra xem nút edit có ẩn không
         WebElement element = driver.findElement(By.cssSelector(".v-btn.v-btn--elevated.v-btn--icon.v-theme--SCREEN_MODE_LIGHT.bg-red.v-btn--density-default.rounded-0.v-btn--size-default.v-btn--variant-elevated.mx-1.text-white"));
         Assert.assertFalse(element.isDisplayed(), "Phần tử vẫn hiển thị!");
